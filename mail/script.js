f1c4ebebd3265f28fca107896f316cd9aac755b0
Samuele Miotto <miotto.samuele@gmail.com>
2021-10-26 17:16:56 +0200

    changed 1/0 to true/false in emailCheck

--- mail/script.js
+++ mail/script.js
@@ -7,17 +7,17 @@ let userEmail = prompt("Inserisci la tua email");
 console.log(userEmail);
 
 // confrontare la mail inserita dall'utente con tutte le email inserite nella lista
-let emailCheck = 0;
+let emailCheck = false;
 
 for (let i=0; i < emailList.length; i++) {
     if (emailList[i] === userEmail) {
-        emailCheck = 1;
+        emailCheck = true;
     }
 };
 
 // se la mail è presente nella lista l'utente può accedere, se non è presente l'utente viene bloccato
 if (emailCheck == 1) {
     console.log("la tua email è presente nel database");
-} else if (emailCheck == 0) {
+} else {
     console.log("la tua email non esiste");
 }
